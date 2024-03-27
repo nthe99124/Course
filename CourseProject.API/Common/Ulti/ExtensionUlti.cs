b549d9930d4668eddba2dd89ba3c3215b906fed1
TheNguyen <nthe99124@gmail.com>
2024-03-27 18:20:17 +0700

    thêm tiếp admin

---
+++ CourseProject.API/Common/Ulti/ExtensionUlti.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace CourseProject.API.Common.Ulti
+{
+    public static class ExtensionUlti
+    {
+        public static Expression<Func<T, bool>> PedicateAnd<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
---
+++ CourseProject.Model/ViewModel/Course/CreateCourseVM.cs
@@ -0,0 +1,7 @@
+﻿namespace CourseProject.Model.ViewModel.Course;
+
+public class CreateCourseVM
+{
+    public string CourseName { get; set; }
+    public Guid? Teacher { get; set; }
+}
